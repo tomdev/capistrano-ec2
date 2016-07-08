dfe2a328e99e478dfafd5ef928d5bd6d2778de12
Tom de Vries <tom@hackerone.com>
2016-07-08 15:28:40 +0200

    Add for_each_ec2_server helper
    I'm not going to lie, that's why the filename is capistrano_monkey_patch

--- lib/capistrano/ec2.rb
+++ lib/capistrano/ec2.rb
@@ -1,4 +1,6 @@
+require "capistrano"
 require "capistrano/ec2/version"
+require "capistrano/ec2/capistrano_monkey_patch"
 
 module Capistrano
   module Ec2
---
+++ lib/capistrano/ec2/capistrano_monkey_patch.rb
@@ -0,0 +1,21 @@
+require 'fog'
+
+module Capistrano
+  class Configuration
+    def for_each_ec2_server(ec2_env:, ec2_role:, &block)
+      ec2 = Fog::Compute.new(provider: 'AWS', region: fetch(:region))
+
+      filters = { "tag:ec2_env" => ec2_env, "tag:role" => ec2_role }
+
+      ec2.servers.all(filters).map.with_index do |ec2_server, index|
+        yield ec2_server, index
+      end
+    end
+  end
+
+  module DSL
+    module Env
+      def_delegators :env, :for_each_ec2_server
+    end
+  end
+end
