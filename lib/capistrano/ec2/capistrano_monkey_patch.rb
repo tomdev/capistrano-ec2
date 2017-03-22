7db41c5678a21e284047a0641083f295ba6cad70
Reed Loden <reed@hackerone.com>
2017-03-22 12:04:52 -0700

    Support the use of IAM instance profiles when connecting to AWS EC2 API
    This adds a new `:use_iam_profile` option that can be set in the Capistrano
    deploy configuration that makes fog attempt to use credentials gained
    from the EC2 metadata service.

--- lib/capistrano/ec2/capistrano_monkey_patch.rb
+++ lib/capistrano/ec2/capistrano_monkey_patch.rb
@@ -3,7 +3,10 @@ require 'fog/aws'
 module Capistrano
   class Configuration
     def for_each_ec2_server(ec2_env:, ec2_role:, &block)
-      ec2 = Fog::Compute.new(provider: 'AWS', region: fetch(:region))
+      ec2 = Fog::Compute.new \
+        provider: 'AWS',
+        region: fetch(:region),
+        use_iam_profile: fetch(:use_iam_profile, false)
 
       filters = { "tag:ec2_env" => ec2_env, "tag:role" => ec2_role }
 
