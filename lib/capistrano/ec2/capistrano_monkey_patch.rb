ddeb5703d40e68d8660d68912db76fae0c52c0b8
Tom de Vries <tom@hackerone.com>
2019-08-14 13:56:41 +0200

    Support local deploying using AWS credentials in env variables

--- lib/capistrano/ec2/capistrano_monkey_patch.rb
+++ lib/capistrano/ec2/capistrano_monkey_patch.rb
@@ -4,8 +4,8 @@ module Capistrano
   class Configuration
     def for_each_ec2_server(ec2_env:, ec2_role:, &block)
       filters = {
-        "tag:ec2_env" => ec2_env,
-        "tag:role" => ec2_role,
+        'tag:ec2_env' => ec2_env,
+        'tag:role' => ec2_role,
         'instance-state-name': 'running'
       }
 
@@ -19,10 +19,40 @@ module Capistrano
     private
 
     def ec2
-      @ec2 ||= Fog::Compute.new \
+      @ec2 ||= Fog::Compute.new(fog_arguments)
+    end
+
+    def fog_arguments
+      default_options = {
         provider: 'AWS',
-        region: fetch(:region),
-        use_iam_profile: fetch(:use_iam_profile, false)
+        region: region,
+      }
+
+      if use_iam_profile
+        default_options.merge(iam_profile_options)
+      else
+        default_options.merge(access_key_options)
+      end
+    end
+
+    def region
+      fetch(:region)
+    end
+
+    def use_iam_profile
+      fetch(:use_iam_profile, false)
+    end
+
+    def iam_profile_options
+      { use_iam_profile: true }
+    end
+
+    def access_key_options
+      {
+        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        aws_session_token: ENV['AWS_SESSION_TOKEN'],
+      }
     end
   end
 
