cc8e1d615948916b2afce36ac631c1dea03318e8
Tom de Vries <tom@hackerone.com>
2019-08-14 13:18:44 +0200

    Extract method and add caching of Compute object

--- lib/capistrano/ec2/capistrano_monkey_patch.rb
+++ lib/capistrano/ec2/capistrano_monkey_patch.rb
@@ -3,15 +3,10 @@ require 'fog/aws'
 module Capistrano
   class Configuration
     def for_each_ec2_server(ec2_env:, ec2_role:, &block)
-      ec2 = Fog::Compute.new \
-        provider: 'AWS',
-        region: fetch(:region),
-        use_iam_profile: fetch(:use_iam_profile, false)
-
-      filters = { 
-        "tag:ec2_env" => ec2_env, 
-        "tag:role" => ec2_role, 
-        'instance-state-name': 'running' 
+      filters = {
+        "tag:ec2_env" => ec2_env,
+        "tag:role" => ec2_role,
+        'instance-state-name': 'running'
       }
 
       ec2.servers.all(filters).map.with_index do |ec2_server, index|
@@ -19,6 +14,15 @@ module Capistrano
 
         yield ec2_server, index
       end
+    end
+
+    private
+
+    def ec2
+      @ec2 ||= Fog::Compute.new \
+        provider: 'AWS',
+        region: fetch(:region),
+        use_iam_profile: fetch(:use_iam_profile, false)
     end
   end
 
